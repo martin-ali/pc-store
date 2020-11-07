c8e0b23878b3c0ff776c95bbb9929a178b52bf75
Martin <marto7395@gmail.com>
2020-11-07 21:48:22 +0200

    Added initial migrations

--- PcStore/App/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ PcStore/App/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace App.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(PcStoreDbContext))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
