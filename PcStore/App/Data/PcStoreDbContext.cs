6bd7309b68a09b9f3bd0233a81dc8806f10261c1
Martin <marto7395@gmail.com>
2020-11-07 21:47:09 +0200

    Renamed ApplicationDbContext to PcStoreDbContext

---
+++ PcStore/App/Data/PcStoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace App.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+    using App.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class PcStoreDbContext : IdentityDbContext
+    {
+        public PcStoreDbContext(DbContextOptions<PcStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Processor> Processors { get; set; }
+    }
+}
