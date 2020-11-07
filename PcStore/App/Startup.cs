2f280cb473614c61f4d975afaed8291cd2c251da
Martin <marto7395@gmail.com>
2020-11-07 21:49:18 +0200

    Changed default error page

--- PcStore/App/Startup.cs
+++ PcStore/App/Startup.cs
@@ -46,7 +46,7 @@ namespace App
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
