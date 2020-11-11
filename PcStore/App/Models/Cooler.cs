9bfbe11fc1641f16a528334c1a505e4644448327
Martin <marto7395@gmail.com>
2020-11-11 20:36:39 +0200

    Added Cooler initial properties

--- PcStore/App/Models/Cooler.cs
+++ PcStore/App/Models/Cooler.cs
@@ -1,7 +1,21 @@
+using App.Enumerations;
+
 namespace App.Models
 {
     public class Cooler
     {
+        public int Id { get; set; }
 
+        public decimal Price { get; set; }
+
+        public int CoolingTdpWatts { get; set; }
+
+        public int SocketId { get; set; }
+
+        public ProcessorSocket Socket { get; set; }
+
+        public int BrandId { get; set; }
+
+        public Brand Brand { get; set; }
     }
 }
