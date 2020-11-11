a6b49564d1d5e3d5434e6627371bc5dd5b666678
Martin <marto7395@gmail.com>
2020-11-11 20:37:42 +0200

    Added StorageDevice initial properties

--- PcStore/App/Models/StorageDevice.cs
+++ PcStore/App/Models/StorageDevice.cs
@@ -1,7 +1,19 @@
 namespace App.Models
 {
+Added Cooler initial propertiesGrapgh    using App.Enumerations;
+
     public class StorageDevice
     {
+        public int Id { get; set; }
 
+        public decimal Price { get; set; }
+
+        public string Model { get; set; }
+
+        public int CapacityBytes { get; set; }
+
+        public int BrandId { get; set; }
+
+        public Brand Brand { get; set; }
     }
 }
