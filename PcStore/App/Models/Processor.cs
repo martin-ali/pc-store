c22e0075a41c03dbe8db4f2094739457dcd28fa5
Martin <marto7395@gmail.com>
2020-11-11 20:07:13 +0200

    Added Processor initial properties

--- PcStore/App/Models/Processor.cs
+++ PcStore/App/Models/Processor.cs
@@ -1,7 +1,15 @@
 namespace App.Models
 {
+    using App.Enumerations;
+
     public class Processor
     {
         public int Id { get; set; }
+
+        public float Frequency { get; set; }
+
+        public int CoresCount { get; set; }
+
+        public ProcessorManufacturer Manufacturer { get; set; }
     }
 }
