3cb446c3b779c6d06e980913a8274040f547491a
Martin <marto7395@gmail.com>
2020-11-11 20:37:17 +0200

    Added Motherboard initial properties

--- PcStore/App/Models/Motherboard.cs
+++ PcStore/App/Models/Motherboard.cs
@@ -1,7 +1,25 @@
 namespace App.Models
 {
+    using App.Enumerations;
+
     public class Motherboard
     {
+        public int Id { get; set; }
 
+        public decimal Price { get; set; }
+
+        public string Model { get; set; }
+
+        public int SocketId { get; set; }
+
+        public ProcessorSocket Socket { get; set; }
+
+        public int BrandId { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public int FormFactorId { get; set; }
+
+        public MotherboardFormFactor FormFactor { get; set; }
     }
 }
