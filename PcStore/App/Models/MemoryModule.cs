2f7dd833964b5ebef021f30c200ef85d66deacb2
Martin <marto7395@gmail.com>
2020-11-11 20:37:05 +0200

    Added MemoryModule initial properties

--- PcStore/App/Models/MemoryModule.cs
+++ PcStore/App/Models/MemoryModule.cs
@@ -1,7 +1,23 @@
 namespace App.Models
 {
+    using App.Enumerations;
+
     public class MemoryModule
     {
+        public int Id { get; set; }
 
+        public decimal Price { get; set; }
+
+        public string Model { get; set; }
+
+        public int CapacityBytes { get; set; }
+
+        public int TypeId { get; set; }
+
+        public MemoryType Type { get; set; }
+
+        public int BrandId { get; set; }
+
+        public Brand Brand { get; set; }
     }
 }
