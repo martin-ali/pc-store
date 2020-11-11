087298872b9e9e43f76a0fdb40cd0ac835f06ea0
Martin <marto7395@gmail.com>
2020-11-11 20:36:58 +0200

    Added GraphicsCard initial properties

--- PcStore/App/Models/GraphicsCard.cs
+++ PcStore/App/Models/GraphicsCard.cs
@@ -1,7 +1,21 @@
 namespace App.Models
 {
+    using App.Enumerations;
+
     public class GraphicsCard
     {
+        public int Id { get; set; }
 
+        public decimal Price { get; set; }
+
+        public string Model { get; set; }
+
+        public int MemoryCapacityBytes { get; set; }
+
+        public float CoreFrequency { get; set; }
+
+        public int BrandId { get; set; }
+
+        public Brand Brand { get; set; }
     }
 }
