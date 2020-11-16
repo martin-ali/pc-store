e0a9828d893112d3fb222b913445f5ef75602c22
Tectonik <marto7395@gmail.com>
2020-11-16 22:08:51 +0200

    Added DbSets for Brands and Products

--- Data/PcStore.Data/ApplicationDbContext.cs
+++ Data/PcStore.Data/ApplicationDbContext.cs
@@ -26,6 +26,10 @@
 
         public DbSet<Setting> Settings { get; set; }
 
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
         public override int SaveChanges() => this.SaveChanges(true);
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
