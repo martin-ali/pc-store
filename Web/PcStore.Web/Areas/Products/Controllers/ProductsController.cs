2eca566fd9f95a14b6fb80694591b723b797f283
Martin <marto7395@gmail.com>
2020-12-17 11:28:55 +0200

    Added ProductsController and test view in Products area

--- Web/PcStore.Web/Areas/Products/Controllers/ProductsController.cs
+++ Web/PcStore.Web/Areas/Products/Controllers/ProductsController.cs
@@ -1,6 +1,26 @@
 namespace PcStore.Web.Areas.Products.Controllers
 {
-    public class ProductsController
+    using Microsoft.AspNetCore.Mvc;
+
+    using PcStore.Common;
+    using PcStore.Data.Common.Models;
+    using PcStore.Data.Models;
+    using PcStore.Services.Data;
+    using PcStore.Web.Controllers;
+
+    [Area(GlobalConstants.ProductsArea)]
+    public class ProductsController : BaseController
     {
+        private readonly IProductsService<Product> productsService;
+
+        public ProductsController(IProductsService<Product> productsService)
+        {
+            this.productsService = productsService;
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
     }
 }
