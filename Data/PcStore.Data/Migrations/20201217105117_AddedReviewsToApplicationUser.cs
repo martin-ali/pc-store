81ad23bb2db6c3b2fd8d06f7e63601d3c0691cee
Martin <marto7395@gmail.com>
2020-12-17 12:56:42 +0200

    Added new migration for Reviews in ApplicationUser

---
+++ Data/PcStore.Data/Migrations/20201217105117_AddedReviewsToApplicationUser.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PcStore.Data.Migrations
+{
+    public partial class AddedReviewsToApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_AuthorId1",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_AuthorId1",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId1",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Reviews",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_AuthorId",
+                table: "Reviews",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_AuthorId",
+                table: "Reviews",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_AuthorId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_AuthorId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Reviews",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorId1",
+                table: "Reviews",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_AuthorId1",
+                table: "Reviews",
+                column: "AuthorId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_AuthorId1",
+                table: "Reviews",
+                column: "AuthorId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
